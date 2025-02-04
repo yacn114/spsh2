921f3672d65826ba1efb640832ce40948433d020
yacnlinux <yacn1214@gmail.com>
2025-02-05 00:18:23 +0330

    first commit 2025 Feb 6 test

--- public/index.php
+++ public/index.php
@@ -9,7 +9,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
